ae925776a525636d7ce98daf0ecc042f2432968b
Adas Alvikas <adas.alvikas@live.com>
2024-05-31 10:30:31 +0100

    hero section

---
+++ src/pages/Home/HeroSection.jsx
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+    return(
+        <section id="heroSection" className="hero-section">
+            <div className="hero-section-content-box">
+                <div className="hero-section-content">
+                    <p className="section-title">Hey, I'm Adas</p>
+                </div>
+            </div>
+        </section>
+    );
+}
