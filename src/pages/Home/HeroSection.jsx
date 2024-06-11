85e5dc9148387ca297358286f4a55f2fa6a4ae62
Adas Alvikas <adas.alvikas@live.com>
2024-06-11 21:52:28 +0300

    hero

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -4,7 +4,23 @@ export default function HeroSection() {
             <div className="hero-section-content-box">
                 <div className="hero-section-content">
                     <p className="section-title">Hey, I'm Adas</p>
+                    <h1 className="hero-section-title">
+                        <span className="hero-section-title-color">
+                            Full Stack
+                        </span>{ " "}
+                        <br/>
+                        Developer
+                    </h1>
+                    <p className="hero-section-description">
+                        Aš myliu Vaidą.
+                        <br/>
+                        Ir visada mylėsiu.
+                    </p>
                 </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+            <div className="hero-section-img">
+                <img src="./img/hero_img.png" style={{maxHeight:300}} alt="Hero Section"/>
             </div>
         </section>
     );
