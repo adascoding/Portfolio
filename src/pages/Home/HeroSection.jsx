73edc8c8e7e5d73292b9e64335b080a5446815df
Adas Alvikas <adas.alvikas@live.com>
2024-06-20 19:41:56 +0300

    updated hero component

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,32 +1,22 @@
 export default function HeroSection() {
-    return(
-        <section id="heroSection" className="hero-section">
-            <div className="hero-section-content-box">
-                <div className="hero-section-content">
-                    <p className="section-title">Hey, I'm Adas</p>
-                    <h1 className="hero-section-title">
-                        <span className="hero-section-title-color">
-                            Junior Software
-                        </span>{ " "}
-                        <br/>
-                        Developer
-                    </h1>
-                    <p className="hero-section-description">
-                    Recent graduate in Software Systems from Kauno kolegija, Lithuania,
-                    with hands-on experience in Agile Scrum methodologies gained
-                    during a junior .NET Developer internship.
-                        <br/>
-                        Eager to apply skills in C#, React.js, .NET Core, and SQL Server
-                        to contribute effectively as a Junior Developer.
-                        Passionate about continuous learning and growth in
-                        software development, seeking opportunities to leverage
-                        technical expertise in a collaborative team environment.
+    return (
+        <section id="hero" className="hero-section">
+            <div className="hero-content">
+            
+                <div className="hero-text">
+                <p className="hero-greeting">Hey, I'm Adas</p>
+                    <h1 className="hero-title">Junior Software <span style={{ color: '#282938' }}>Developer</span></h1>
+                    <p className="hero-description">
+                        Recent graduate in Software Systems from Kauno kolegija, Lithuania, with hands-on experience in Agile Scrum methodologies gained during a junior .NET Developer internship.
                     </p>
+                    <p className="hero-description">
+                        Eager to apply skills in C#, React.js, .NET Core, and SQL Server to contribute effectively as a Junior Developer. Passionate about continuous learning and growth in software development, seeking opportunities to leverage technical expertise in a collaborative team environment.
+                    </p>
+                    <button className="hero-button">Contact me</button>
                 </div>
-                <button className="btn btn-primary">Get In Touch</button>
-            </div>
-            <div className="hero-section-img">
-                <img className="hero-image" src="./img/hero_img.jpg" alt="Hero Section"/>
+                <div className="hero-image">
+                    <img src="./img/hero_img.jpg" alt="Adas" />
+                </div>
             </div>
         </section>
     );
