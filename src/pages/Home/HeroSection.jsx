3b33198417f2b1286ff5e423077bed2c29fa37ce
Adas Alvikas <adas.alvikas@live.com>
2024-06-25 09:26:05 +0300

    hero buttons small screen flex direction

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -6,30 +6,31 @@ export default function HeroSection() {
     return (
         <section id="hero" className="hero-section">
             <div className="hero-content">
-            
+
                 <div className="hero-text">
-                <p className="hero-greeting">Hey, I'm Adas</p>
+                    <p className="hero-greeting">Hey, I'm Adas</p>
                     <h1 className="hero-title">Junior Software <span style={{ color: '#282938' }}>Developer</span></h1>
                     <p className="hero-description">
                         Recent graduate in Software Systems from Kauno kolegija – Higher Education Institution, Lithuania, with hands-on experience in Agile Scrum methodologies gained during a junior .NET Developer internship.
                     </p>
                     <p className="hero-description">
-                    Motivated to apply skills in C#, React.js, .NET Core, and SQL Server to contribute effectively as a Junior Developer. Passionate about continuous learning and growth in software development, seeking opportunities to leverage technical expertise in a collaborative team environment.
+                        Motivated to apply skills in C#, React.js, .NET Core, and SQL Server to contribute effectively as a Junior Developer. Passionate about continuous learning and growth in software development, seeking opportunities to leverage technical expertise in a collaborative team environment.
                     </p>
-                    
-                    <ScrollLink 
-                        to="contact" 
-                        spy={true} 
-                        smooth={true} 
-                        offset={-70} 
-                        duration={500} 
-                        className="hero-button" 
-                    >
-                        Contact me
-                    </ScrollLink>
-                    <a href="../Adas-CV.pdf" className="cv-download" download>
-                        <FontAwesomeIcon icon={faDownload} /> Download CV
-                    </a>
+                    <div className="button-container">
+                        <ScrollLink
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            className="hero-button"
+                        >
+                            Contact me
+                        </ScrollLink>
+                        <a href="../Adas-CV.pdf" className="cv-download" download>
+                            <FontAwesomeIcon icon={faDownload} /> Download CV
+                        </a>
+                    </div>
 
                 </div>
                 <div className="hero-image">
